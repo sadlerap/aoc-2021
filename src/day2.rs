d7b4136e917047a8bb9e278d333efd3c61f094cf
Andy Sadler <andrewsadler122@gmail.com>
2021-12-03 11:22:50 -0600

    Day 2, part 2
    Signed-off-by: Andy Sadler <andrewsadler122@gmail.com>

--- src/day2.rs
+++ src/day2.rs
@@ -8,10 +8,32 @@ fn part1(input: &str) -> Result<i32, Box<dyn Error>> {
     let mut pos = 0;
     for s in input.split('\n') {
         let (cmd, val) = s.split_once(' ').unwrap_or_default();
+        let val = val.parse::<i32>()?;
         match cmd {
-            "up" => {depth -= val.parse::<i32>()?}
-            "down" => {depth += val.parse::<i32>()?}
-            "forward" => {pos += val.parse::<i32>()?}
+            "up" => depth -= val,
+            "down" => depth += val,
+            "forward" => pos += val,
+            _ => {}
+        }
+    }
+    Ok(depth * pos)
+}
+
+#[aoc(day2, part2)]
+fn part2(input: &str) -> Result<i32, Box<dyn Error>> {
+    let mut depth = 0;
+    let mut pos = 0;
+    let mut aim = 0;
+    for s in input.split('\n') {
+        let (cmd, val) = s.split_once(' ').unwrap_or_default();
+        let val = val.parse::<i32>()?;
+        match cmd {
+            "up" => aim -= val,
+            "down" => aim += val,
+            "forward" => {
+                pos += val;
+                depth += aim * val;
+            }
             _ => {}
         }
     }
